4a2232a2d2ccde87ff078b12472d41cee4d52f07
Ognjen <ognjen@palija.net>
2022-06-10 15:54:29 +0200

    removed comments

--- src/server/auditorium/dto/create-auditorium.dto.ts
+++ src/server/auditorium/dto/create-auditorium.dto.ts
@@ -13,13 +13,11 @@ export class CreateAuditoriumDto {
   @IsOptional()
   theatre: String;
 
-  //@IsNumberString({}, { message: 'Rows parameter is missing or malformed' })
   @IsNumber()
   @Min(1)
   rows: Number;
 
   @IsNumber()
   @Min(1)
-  //@IsNumberString({}, { message: 'Columns parameter is missing or malformed' })
   columns: Number;
 }
